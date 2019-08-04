502047a77c25227acaa622c889e2d44fef67008d
Hecate2112 <dyana.21.7@gmail.com>
2019-08-04 20:00:49 +0200

    Ejercicio 3

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const domain = 'http://localhost:3001'
 
 class App extends PureComponent {
   state = {
+    teams: [],
     players: []
   }
 
@@ -28,29 +29,53 @@ class App extends PureComponent {
   }
 
   render() {
-    const { players } = this.state
+    const { teams, players } = this.state
 
     return <div className="App">
       <header className="App-heading App-flex">
         <h2>Bienvenido a la prueba de los equipos</h2>
       </header>
       <div className="App-teams App-flex">
-        {/* 
-          TODO ejercicio 2
-          Debes obtener los players en lugar de los equipos y pintar su nombre. 
-          Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
-          y una lista con sus nombres. 
-          ** Los comentarios de los ejercicios no los borres.
-        */}
         <h3>Los jugadores:</h3>
-        <ul className="App-players">
-          {/* 
-            TODO ejercicio 3
-            Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
-            Guiate por las imágenes.
-           */}
-          {players.map((player, index) => <li key={index} >{player.name}</li>)}
-        </ul>
+
+        {/* 
+            TODO ejercicio 2
+            Debes obtener los players en lugar de los equipos y pintar su nombre. 
+            Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
+            y una lista con sus nombres. 
+            ** Los comentarios de los ejercicios no los borres.
+          */}
+        {/* 
+            <ul className="App-players">
+              {players.map((player, index) => <li key={index} >{player.name}</li>)} 
+            </ul>
+          */}
+
+
+
+        {/* 
+          TODO ejercicio 3
+          Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
+          Guiate por las imágenes.
+        */}
+        <div className="App-player-container">
+          {players.map((player, index) => {
+            const team = teams.find(team => team.id === player.teamId);
+            return (
+              <div className="App-player-card" key={index}>
+                <img className="App-team-shield" src={team.shield} />
+                <img className="App-team-player" src={player.img} />
+                <p>
+                  <span className="App-player-name">{player.name}&nbsp;</span>
+                  <span className="App-player-position">{player.position}</span>
+                </p>
+                <p className="App-player-team">{team.name || ''}</p>
+              </div>)
+          })}
+        </div>
+
+
+
       </div>
       <div className="App-instructions App-flex">
         <img className="App-logo" src={reactSvg}/>
